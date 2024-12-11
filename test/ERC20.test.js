0f8aca9975397f0c267fed2fd2ca46b5ae48d6d1
resgjk <aremijplohih@gmail.com>
2024-12-11 23:06:58 +0500

    feat: Add test for mint

--- test/ERC20.test.js
+++ test/ERC20.test.js
@@ -14,7 +14,7 @@ describe("ERC20", () => {
 	beforeEach(async () => {
 		[owner, user] = await ethers.getSigners();
 		const tokenContract = await ethers.getContractFactory(
-			"TokenContractTokenContract",
+			"TokenContract",
 			owner
 		);
 		token = await tokenContract.deploy();
@@ -34,6 +34,35 @@ describe("ERC20", () => {
 	});
 
 	describe("_mint", () => {
-		it("zero-based shipping")
+		it("does not mint on a zero account", async () => {
+			await expect(token._mint(ethers.ZeroAddress, initialSupply))
+				.to.be.revertedWithCustomError(token, "ERC20InvalidReceiver")
+				.withArgs(ethers.ZeroAddress);
+		});
+
+		describe("mint to non-zero account", () => {
+			let tx;
+			beforeEach("minting", async () => {
+				tx = await token.connect(owner)._mint(user, initialSupply);
+			});
+
+			it("check total supply", async () => {
+				expect(await token.totalSupply()).to.equal(initialSupply);
+			});
+
+			it("check user balance", async () => {
+				await expect(tx).to.changeTokenBalance(token, user, initialSupply);
+			});
+
+			it("emits Transfer event", async () => {
+				await expect(tx)
+					.to.emit(token, "Transfer")
+					.withArgs(ethers.ZeroAddress, user, initialSupply);
+			});
+		});
 	});
+
+	// describe("_burn", () => {
+
+	// });
 });
